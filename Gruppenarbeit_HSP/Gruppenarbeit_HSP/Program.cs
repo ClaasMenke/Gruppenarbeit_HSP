9da898fdf74e3ba1991c2f7134fde776f4ee5792
ClaasMenke <claas98.cm@gmail.com>
2020-10-29 14:41:49 +0100

    Update Program.cs

--- Gruppenarbeit_HSP/Gruppenarbeit_HSP/Program.cs
+++ Gruppenarbeit_HSP/Gruppenarbeit_HSP/Program.cs
@@ -4,9 +4,45 @@ namespace Gruppenarbeit_HSP
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
+            static void Main(string[] args)
+            {
+                double m, p, c, hf, ha, df, db, d;
+                int angle = 20;
+
+                //Eingabe
+                Console.WriteLine("Bitte geben Sie das Modul an: ");
+                m = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Bitte geben Sie den Teilkreisdurchmesser an: ");
+                d = Convert.ToInt32(Console.ReadLine());
+
+                //Kopfspiel
+                c = 0.167 * m;
+
+                //Zahnfußhöhe
+                hf = m + c;
+
+                //Zahnkopfhöhe
+                ha = m;
+
+                //Teilung
+                p = Math.PI * m;
+
+                //Fußkreisdurchmesser
+                df = d - 2 * (m + c);
+
+                //Grundkreisdurchmesser
+                db = d * Math.Cos(angle);
+
+                //Ausgabe
+                Console.WriteLine("Zahnfußhöhe: " + hf);
+                Console.WriteLine("Zahnkopfhöhe: " + ha);
+                Console.WriteLine("Teilung: " + p);
+                Console.WriteLine("Fußkreisdurchmesser: " + df);
+                Console.WriteLine("Grundkreisdurchmesser: " + db);
+                Console.ReadKey();
+
+            }
     }
 }
