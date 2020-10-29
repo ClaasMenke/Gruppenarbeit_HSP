d23cf453f1705243fbe470bd664d9a5c791243d3
ClaasMenke <claas98.cm@gmail.com>
2020-10-29 14:37:03 +0100

    Initial Commit

---
+++ Gruppenarbeit_HSP/Gruppenarbeit_HSP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gruppenarbeit_HSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
