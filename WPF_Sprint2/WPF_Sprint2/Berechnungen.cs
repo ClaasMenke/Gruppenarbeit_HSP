3b43e76441322e3748bc0df683b8757424143378
ClaasMenke <claas98.cm@gmail.com>
2020-11-21 13:42:00 +0100

    .

---
+++ WPF_Sprint2/WPF_Sprint2/Berechnungen.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Sprint2
+{
+    class Berechnungen
+    {
+        //Teilung
+        public double Teilung(double m)     // public double Teilung = die erstellung einer Variablen, Datentyp schließt Kommazahlen mit ein
+        {                                   // m = Modul
+            double p = Math.PI * m;
+            return Math.Round(p, 2);        // Ausgabe von p auf zwei Nachkommastellen gerundet
+        }
+
+        //Kopfspiel
+        public double Kopfspiel(double m)
+        {
+            double c = 0.167 * m;           // 0.167 * m angenommen, da am häufigsten verwendet
+            return Math.Round(c, 2);
+        }
+
+        //Kopfkreisdurchmesser außenverzahnt
+        public double Kopfkreisdurchmesser_a(double m, double z)    // z = Zähnezahl
+        {
+            double da = m * (z + 2);
+            return Math.Round(da, 2);
+        }
+
+        //Fußkreisdurchmesser außenverzahnt 
+        public double Fußkreisdurchmesser_a(double d, double m, double c)   // d = Teilkreisdurchmesser
+        {                                                                   // c = Kopfspiel
+            double df = d - 2 * (m + c);
+            return Math.Round(df, 2);
+        }
+
+        //Kopfkreisdurchmesser innenverzahnt
+        public double Kopfkreisdurchmesser_i(double m, double z)
+        {
+            double da = m * (z - 2);
+            return Math.Round(da, 2);
+        }
+
+        //Fußkreisdurchmesser innenverzahnt
+        public double Fußkreisdurchmesser_i(double d, double m, double c)
+        {
+            double df = d + 2 * (m + c);
+            return Math.Round(df, 2);
+        }
+
+        //Grundkreisdurchmesser
+        public double Grundkreisdurchmesser(double d)
+        {
+            double nw = 20 * Math.PI / 180;     // nw = Normaleingriffswinkel (20 Grad nach Normverzahnung)
+            double dg = d * Math.Cos(nw);       // dg = Grundkreisdurchmesser
+            return Math.Round(dg, 2);
+        }
+
+        //Zahnkopfhöhe
+        public double Zahnkopfhöhe(double m)
+        {
+            double ha = m;
+            return Math.Round(ha, 2);
+        }
+
+        //Zahnfußhöhe
+        public double Zahnfußhöhe(double m, double c)
+        {
+            double hf = m + c;
+            return Math.Round(hf, 2);
+        }
+
+        //Zahnhöhe
+        public double Zahnhöhe(double m, double c)
+        {
+            double h = 2 + m + c;
+            return Math.Round(h, 2);
+        }
+
+        //Volumen
+        public double Volumen(double da, double b)  // da = zuvor berechneter Kopfkreisdurchmesser
+        {                                           // b = Breite Zahnrad
+            double v = Math.Pow(da / 2, 2) * b;     // Math.pow = da/2 wird potenziert mit 2 als Exponenten
+            return Math.Round(v, 2);
+        }
+
+        //Achsabstand mit außenliegendem Gegenrad
+        public double Achsabstand_a(double d1, double d2)   // d1 = Durchmesser Zahnrad 1/ d2 = für Zr 2
+        {
+            double aa = (d2 + d1) / 2;
+            return Math.Round(aa, 2);
+        }
+
+        //Achsabstand mit innenliegendem Gegenrad
+        public double Achsabstand_i(double d1, double d2)
+        {
+            double ai = (d2 - d1) / 2;
+            return Math.Round(ai, 2);
+        }
+    }
+}
--- WPF_Sprint2/WPF_Sprint2/MainWindow.xaml.cs
+++ WPF_Sprint2/WPF_Sprint2/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace WPF_Sprint2
     /// </summary>
     public partial class MainWindow : Window
     {
+        Berechnungen brg = new Berechnungen();
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +31,12 @@ namespace WPF_Sprint2
         {
             tbc_innen.Visibility = Visibility.Hidden;
             tbc_außen.Visibility = Visibility.Visible;
+
+            btn_Berechne_außenEinzel.Visibility = Visibility.Visible;
+            btn_Berechne_außenGegenrad.Visibility = Visibility.Hidden;
+            btn_Berechne_innenEinzel.Visibility = Visibility.Hidden;
+            btn_Berechne_innenGegenrad.Visibility = Visibility.Hidden;
+
             grd_EingabeAußenEinzel.Visibility = Visibility.Visible;
             grd_EingabeAußenGegenrad1.Visibility = Visibility.Hidden;
             grd_EingabeAußenGegenrad2.Visibility = Visibility.Hidden;
@@ -42,6 +49,12 @@ namespace WPF_Sprint2
         {
             tbc_innen.Visibility = Visibility.Hidden;
             tbc_außen.Visibility = Visibility.Visible;
+
+            btn_Berechne_außenEinzel.Visibility = Visibility.Hidden;
+            btn_Berechne_außenGegenrad.Visibility = Visibility.Visible;
+            btn_Berechne_innenEinzel.Visibility = Visibility.Hidden;
+            btn_Berechne_innenGegenrad.Visibility = Visibility.Hidden;
+
             grd_EingabeAußenGegenrad1.Visibility = Visibility.Visible;
             grd_EingabeAußenGegenrad2.Visibility = Visibility.Visible;
             grd_EingabeAußenEinzel.Visibility = Visibility.Hidden;
@@ -54,6 +67,12 @@ namespace WPF_Sprint2
         {
             tbc_außen.Visibility = Visibility.Hidden;
             tbc_innen.Visibility = Visibility.Visible;
+
+            btn_Berechne_außenEinzel.Visibility = Visibility.Hidden;
+            btn_Berechne_außenGegenrad.Visibility = Visibility.Hidden;
+            btn_Berechne_innenEinzel.Visibility = Visibility.Visible;
+            btn_Berechne_innenGegenrad.Visibility = Visibility.Hidden;
+
             grd_EingabeInnenEinzel.Visibility = Visibility.Visible;
             grd_EingabeInnenGegenrad1.Visibility = Visibility.Hidden;
             grd_EingabeInnenGegenrad2.Visibility = Visibility.Hidden;
@@ -66,6 +85,12 @@ namespace WPF_Sprint2
         {
             tbc_außen.Visibility = Visibility.Hidden;
             tbc_innen.Visibility = Visibility.Visible;
+
+            btn_Berechne_außenEinzel.Visibility = Visibility.Hidden;
+            btn_Berechne_außenGegenrad.Visibility = Visibility.Hidden;
+            btn_Berechne_innenEinzel.Visibility = Visibility.Hidden;
+            btn_Berechne_innenGegenrad.Visibility = Visibility.Visible;
+
             grd_EingabeInnenEinzel.Visibility = Visibility.Hidden;
             grd_EingabeInnenGegenrad1.Visibility = Visibility.Visible;
             grd_EingabeInnenGegenrad2.Visibility = Visibility.Visible;
@@ -77,6 +102,36 @@ namespace WPF_Sprint2
         private void btn_Exit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        // Berechnungen für ein einzelnes außenverzahntes Stirnrad
+        private void btn_Berechne_außenEinzel_Click_1(object sender, RoutedEventArgs e)
+        {
+            double m1 = Convert.ToDouble(tbx_Modul_außenEinzel.Text);
+            int z1 = Convert.ToInt32(tbx_Zaehnezahl_außenEinzel.Text);
+            double d1 = Convert.ToDouble(tbx_Teilkreisdurchmesser_außenEinzel.Text);
+            double b1 = Convert.ToDouble(tbx_Breite_außenEinzel.Text);
+
+
+            //Ergebnisse Zahnrad1 
+            double p1 = brg.Teilung(m1);
+            lab_Teilung_außenEinzelErgebnis.Content = p1 + " mm";
+            double c1 = brg.Kopfspiel(m1);
+            lab_Kopfspiel_außenEinzelErgebnis.Content = c1 + " mm";
+            double da1 = brg.Kopfkreisdurchmesser_a(m1, z1);
+            lab_Kopfkreisdurchmesser_außenEinzelErgebnis.Content = da1 + " mm";
+            double df1 = brg.Fußkreisdurchmesser_a(d1, m1, c1);
+            lab_Fußkreisdurchmesser_außenEinzelErgebnis.Content = df1 + " mm";
+            double dg1 = brg.Grundkreisdurchmesser(d1);
+            lab_Grundkreisdurchmesser_außenEinzelErgebnis.Content = dg1 + " mm";
+            double ha1 = brg.Zahnkopfhöhe(m1);
+            lab_Zahnkopfhoehe_außenEinzelErgebnis.Content = ha1 + " mm";
+            double hf1 = brg.Zahnfußhöhe(m1, c1);
+            lab_Zahnfußhoehe_außenEinzelErgebnis.Content = hf1 + " mm";
+            double h1 = brg.Zahnhöhe(m1, c1);
+            lab_Zahnhoehe_außenEinzelErgebnis.Content = h1 + " mm";
+            double v1 = brg.Volumen(da1, b1);
+            lab_Volumen_außenEinzelErgebnis.Content = v1 + " mm^3";
         }
     }
 }
